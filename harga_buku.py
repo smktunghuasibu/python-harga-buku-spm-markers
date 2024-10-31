0ecbc21b11a2c99ae291c80f555361d9dcd53cda
Albee-Cheng-Jing-Xuan <163367736+Albee-Cheng-Jing-Xuan@users.noreply.github.com>
2024-11-01 07:54:27 +0800

    Update harga_buku.py

--- harga_buku.py
+++ harga_buku.py
@@ -1,11 +1,16 @@
-def harga_bayaran(.........):
-    
-
-
-
-
-
-    return 
+def harga_bayaran(jenisbuku, kuantiti):
+    if jenisbuku==1:
+        harga_seunit=6.00
+        kadardiskaun=10
+    elif jenisbuku==2:
+        harga_seunit=7.50
+        kadardiskaun=8
+    else: 
+        harga_seunit=8.90
+        kadardiskaun=5
+    potongan_harga=kuantiti*harga_seunit*(kadardiskaun/100)
+    harga_total=(kuantiti*harga_seunit)-potongan_harga
+    return potongan_harga,harga_total
 
 def main():
     print("Senarai belian buku:")
